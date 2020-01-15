855108f36a4993384948efd7c72522cc3e4069a6
poquitosekki <aymanbelhaj01@gmail.com>
2020-01-15 10:40:10 +0100

    first commit

---
+++ morse.py
@@ -0,0 +1,24 @@
+import csv
+
+path = "/Users/poquito/Desktop/"
+
+def mtos(m):
+	m = m.split(" ")
+	print(m)
+	result = []
+	with open(f"{path}data.csv", "r") as f:
+		mread = csv.reader(f)
+		for char in m:
+			if char == "/":
+				result.append(" ")
+				continue
+			# the second loop (iterating through the CSV file)
+			for row in mread:
+				if char == row[1]:
+					result.append(row[0])
+					f.seek(0)
+					break
+	return "".join(result)
+
+str = input("Enter your morse code : ")
+print(mtos(str))
